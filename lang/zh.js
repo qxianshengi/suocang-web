a9e1241478f77efcc37be4a980607abbbb8ace63
qxianshengi <37892250+qxianshengi@users.noreply.github.com>
2018-04-13 09:44:17 +0800

    Add files via upload

---
+++ lang/zh.js
@@ -0,0 +1,38 @@
+   module.exports = {
+       header: {
+           login: '登录',
+           reg: '注册',
+           logout: '退出登录',
+           totalAssets: '折合总资产',
+           deposit: '保证金',
+           currency: '币种',
+           available: '可用',
+           freeze: '冻结',
+           recharge: '充值',
+           withdrawal: '提现',
+       },
+       nav: {
+           homePage: '首页',
+           tradingCenter: '交易中心',
+           orderCenter: '订单中心',
+           financialManagement: '钱包地址',
+           certifiedBusiness: '锁仓',
+           userCenter: '用户中心',
+           advertise: '官方公告',
+       },
+       footer: {
+           commonProblem: '常见问题',
+           usageAgreement: '使用协议',
+           rateDescription: '费率说明',
+           mailbox: '联系邮箱',
+           contact:'联系我们'
+       },
+       financeNav: {
+           myAssets: '我的资产'
+       },
+       login:{
+          Welcomelanding:'欢迎登录',
+          questionfirst:'Decred中文含义',
+          questionsecond:'Decred的发行量是多少'
+       }
+   }
